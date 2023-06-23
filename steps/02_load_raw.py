ba839a5cba06da8027017bcd18ba54b8f469521c
cristianurcuqui <cristianurcuqui@gmail.com>
2023-06-22 23:34:18 -0500

    prueba2

--- steps/02_load_raw.py
+++ steps/02_load_raw.py
@@ -5,58 +5,58 @@
 # Last Updated: 1/9/2023
 #------------------------------------------------------------------------------
 
+# Importa las librerías necesarias.
 import time
 from snowflake.snowpark import Session
-#import snowflake.snowpark.types as T
-#import snowflake.snowpark.functions as F
 
-
+# Define las tablas que vamos a usar.
 POS_TABLES = ['country', 'franchise', 'location', 'menu', 'truck', 'order_header', 'order_detail']
 CUSTOMER_TABLES = ['customer_loyalty']
+
+# Crea un diccionario con los esquemas y las tablas correspondientes.
 TABLE_DICT = {
     "pos": {"schema": "RAW_POS", "tables": POS_TABLES},
     "customer": {"schema": "RAW_CUSTOMER", "tables": CUSTOMER_TABLES}
 }
 
-# SNOWFLAKE ADVANTAGE: Schema detection
-# SNOWFLAKE ADVANTAGE: Data ingestion with COPY
-# SNOWFLAKE ADVANTAGE: Snowflake Tables (not file-based)
-
 def load_raw_table(session, tname=None, s3dir=None, year=None, schema=None):
+    # Establece el esquema a usar en la sesión de Snowflake.
     session.use_schema(schema)
+    
+    # Configura la ubicación del archivo en base al año.
     if year is None:
         location = "@external.frostbyte_raw_stage/{}/{}".format(s3dir, tname)
     else:
         print('\tLoading year {}'.format(year)) 
         location = "@external.frostbyte_raw_stage/{}/{}/year={}".format(s3dir, tname, year)
     
-    # we can infer schema using the parquet read option
-    df = session.read.option("compression", "snappy") \
-                            .parquet(location)
+    # Carga los datos del archivo en un DataFrame.
+    df = session.read.option("compression", "snappy").parquet(location)
+    
+    # Copia los datos del DataFrame en la tabla de Snowflake.
     df.copy_into_table("{}".format(tname))
 
-# SNOWFLAKE ADVANTAGE: Warehouse elasticity (dynamic scaling)
-
 def load_all_raw_tables(session):
+    # Aumenta el tamaño del almacén de Snowflake para mejorar el rendimiento.
     _ = session.sql("ALTER WAREHOUSE HOL_WH SET WAREHOUSE_SIZE = XLARGE WAIT_FOR_COMPLETION = TRUE").collect()
 
+    # Carga todas las tablas en Snowflake.
     for s3dir, data in TABLE_DICT.items():
         tnames = data['tables']
         schema = data['schema']
         for tname in tnames:
             print("Loading {}".format(tname))
-            # Only load the first 3 years of data for the order tables at this point
-            # We will load the 2022 data later in the lab
             if tname in ['order_header', 'order_detail']:
                 for year in ['2019', '2020', '2021']:
                     load_raw_table(session, tname=tname, s3dir=s3dir, year=year, schema=schema)
             else:
                 load_raw_table(session, tname=tname, s3dir=s3dir, schema=schema)
 
+    # Devuelve el tamaño del almacén de Snowflake a su tamaño original.
     _ = session.sql("ALTER WAREHOUSE HOL_WH SET WAREHOUSE_SIZE = XSMALL").collect()
 
 def validate_raw_tables(session):
-    # check column names from the inferred schema
+    # Verifica los nombres de las columnas de las tablas.
     for tname in POS_TABLES:
         print('{}: \n\t{}\n'.format(tname, session.table('RAW_POS.{}'.format(tname)).columns))
 
@@ -64,18 +64,20 @@ def validate_raw_tables(session):
         print('{}: \n\t{}\n'.format(tname, session.table('RAW_CUSTOMER.{}'.format(tname)).columns))
 
 
-# For local debugging
+# Esta parte del código se ejecuta si el script se ejecuta directamente (no se importa como módulo).
 if __name__ == "__main__":
-    # Add the utils package to our path and import the snowpark_utils function
     import os, sys
     current_dir = os.getcwd()
     parent_dir = os.path.dirname(current_dir)
     sys.path.append(parent_dir)
 
     from utils import snowpark_utils
+    # Obtiene la sesión de Snowpark.
     session = snowpark_utils.get_snowpark_session()
 
+    # Llama a las funciones para cargar y validar las tablas.
     load_all_raw_tables(session)
-#    validate_raw_tables(session)
+    #validate_raw_tables(session)
 
+    # Cierra la sesión.
     session.close()
--- steps/04_create_pos_view.py
+++ steps/04_create_pos_view.py
@@ -10,113 +10,118 @@
 # SNOWFLAKE ADVANTAGE: Streams on views
 
 
+# Importa la clase Session de Snowflake y el módulo de funciones.
 from snowflake.snowpark import Session
-#import snowflake.snowpark.types as T
 import snowflake.snowpark.functions as F
 
-
+# Define la función para crear la vista POS.
 def create_pos_view(session):
+    # Establece el esquema a usar en la sesión de Snowflake.
     session.use_schema('HARMONIZED')
-    order_detail = session.table("RAW_POS.ORDER_DETAIL").select(F.col("ORDER_DETAIL_ID"), \
-                                                                F.col("LINE_NUMBER"), \
-                                                                F.col("MENU_ITEM_ID"), \
-                                                                F.col("QUANTITY"), \
-                                                                F.col("UNIT_PRICE"), \
-                                                                F.col("PRICE"), \
+    
+    # Selecciona columnas específicas de diferentes tablas.
+    # Aquí estamos seleccionando columnas de la tabla ORDER_DETAIL.
+    order_detail = session.table("RAW_POS.ORDER_DETAIL").select(F.col("ORDER_DETAIL_ID"), 
+                                                                F.col("LINE_NUMBER"), 
+                                                                F.col("MENU_ITEM_ID"), 
+                                                                F.col("QUANTITY"), 
+                                                                F.col("UNIT_PRICE"), 
+                                                                F.col("PRICE"), 
                                                                 F.col("ORDER_ID"))
-    order_header = session.table("RAW_POS.ORDER_HEADER").select(F.col("ORDER_ID"), \
-                                                                F.col("TRUCK_ID"), \
-                                                                F.col("ORDER_TS"), \
-                                                                F.to_date(F.col("ORDER_TS")).alias("ORDER_TS_DATE"), \
-                                                                F.col("ORDER_AMOUNT"), \
-                                                                F.col("ORDER_TAX_AMOUNT"), \
-                                                                F.col("ORDER_DISCOUNT_AMOUNT"), \
-                                                                F.col("LOCATION_ID"), \
+    # Selecciona columnas de la tabla ORDER_HEADER.
+    order_header = session.table("RAW_POS.ORDER_HEADER").select(F.col("ORDER_ID"), 
+                                                                F.col("TRUCK_ID"), 
+                                                                F.col("ORDER_TS"), 
+                                                                F.to_date(F.col("ORDER_TS")).alias("ORDER_TS_DATE"), 
+                                                                F.col("ORDER_AMOUNT"), 
+                                                                F.col("ORDER_TAX_AMOUNT"), 
+                                                                F.col("ORDER_DISCOUNT_AMOUNT"), 
+                                                                F.col("LOCATION_ID"), 
                                                                 F.col("ORDER_TOTAL"))
-    truck = session.table("RAW_POS.TRUCK").select(F.col("TRUCK_ID"), \
-                                                F.col("PRIMARY_CITY"), \
-                                                F.col("REGION"), \
-                                                F.col("COUNTRY"), \
-                                                F.col("FRANCHISE_FLAG"), \
-                                                F.col("FRANCHISE_ID"))
-    menu = session.table("RAW_POS.MENU").select(F.col("MENU_ITEM_ID"), \
-                                                F.col("TRUCK_BRAND_NAME"), \
-                                                F.col("MENU_TYPE"), \
+    # Selecciona columnas de la tabla TRUCK.
+    truck = session.table("RAW_POS.TRUCK").select(F.col("TRUCK_ID"), 
+                                                  F.col("PRIMARY_CITY"), 
+                                                  F.col("REGION"), 
+                                                  F.col("COUNTRY"), 
+                                                  F.col("FRANCHISE_FLAG"), 
+                                                  F.col("FRANCHISE_ID"))
+    # Selecciona columnas de la tabla MENU.
+    menu = session.table("RAW_POS.MENU").select(F.col("MENU_ITEM_ID"), 
+                                                F.col("TRUCK_BRAND_NAME"), 
+                                                F.col("MENU_TYPE"), 
                                                 F.col("MENU_ITEM_NAME"))
-    franchise = session.table("RAW_POS.FRANCHISE").select(F.col("FRANCHISE_ID"), \
-                                                        F.col("FIRST_NAME").alias("FRANCHISEE_FIRST_NAME"), \
-                                                        F.col("LAST_NAME").alias("FRANCHISEE_LAST_NAME"))
+    # Selecciona columnas de la tabla FRANCHISE.
+    franchise = session.table("RAW_POS.FRANCHISE").select(F.col("FRANCHISE_ID"), 
+                                                          F.col("FIRST_NAME").alias("FRANCHISEE_FIRST_NAME"), 
+                                                          F.col("LAST_NAME").alias("FRANCHISEE_LAST_NAME"))
+    # Selecciona la columna LOCATION_ID de la tabla LOCATION.
     location = session.table("RAW_POS.LOCATION").select(F.col("LOCATION_ID"))
 
-    
-    '''
-    We can do this one of two ways: either select before the join so it is more explicit, or just join on the full tables.
-    The end result is the same, it's mostly a readibility question.
-    '''
-    # order_detail = session.table("RAW_POS.ORDER_DETAIL")
-    # order_header = session.table("RAW_POS.ORDER_HEADER")
-    # truck = session.table("RAW_POS.TRUCK")
-    # menu = session.table("RAW_POS.MENU")
-    # franchise = session.table("RAW_POS.FRANCHISE")
-    # location = session.table("RAW_POS.LOCATION")
-
+    # Realiza joins entre las tablas.
     t_with_f = truck.join(franchise, truck['FRANCHISE_ID'] == franchise['FRANCHISE_ID'], rsuffix='_f')
     oh_w_t_and_l = order_header.join(t_with_f, order_header['TRUCK_ID'] == t_with_f['TRUCK_ID'], rsuffix='_t') \
                                 .join(location, order_header['LOCATION_ID'] == location['LOCATION_ID'], rsuffix='_l')
     final_df = order_detail.join(oh_w_t_and_l, order_detail['ORDER_ID'] == oh_w_t_and_l['ORDER_ID'], rsuffix='_oh') \
                             .join(menu, order_detail['MENU_ITEM_ID'] == menu['MENU_ITEM_ID'], rsuffix='_m')
-    final_df = final_df.select(F.col("ORDER_ID"), \
-                            F.col("TRUCK_ID"), \
-                            F.col("ORDER_TS"), \
-                            F.col('ORDER_TS_DATE'), \
-                            F.col("ORDER_DETAIL_ID"), \
-                            F.col("LINE_NUMBER"), \
-                            F.col("TRUCK_BRAND_NAME"), \
-                            F.col("MENU_TYPE"), \
-                            F.col("PRIMARY_CITY"), \
-                            F.col("REGION"), \
-                            F.col("COUNTRY"), \
-                            F.col("FRANCHISE_FLAG"), \
-                            F.col("FRANCHISE_ID"), \
-                            F.col("FRANCHISEE_FIRST_NAME"), \
-                            F.col("FRANCHISEE_LAST_NAME"), \
-                            F.col("LOCATION_ID"), \
-                            F.col("MENU_ITEM_ID"), \
-                            F.col("MENU_ITEM_NAME"), \
-                            F.col("QUANTITY"), \
-                            F.col("UNIT_PRICE"), \
-                            F.col("PRICE"), \
-                            F.col("ORDER_AMOUNT"), \
-                            F.col("ORDER_TAX_AMOUNT"), \
-                            F.col("ORDER_DISCOUNT_AMOUNT"), \
+    
+    # Selecciona columnas específicas del dataframe final.
+    final_df = final_df.select(F.col("ORDER_ID"), 
+                            F.col("TRUCK_ID"), 
+                            F.col("ORDER_TS"), 
+                            F.col('ORDER_TS_DATE'), 
+                            F.col("ORDER_DETAIL_ID"), 
+                            F.col("LINE_NUMBER"), 
+                            F.col("TRUCK_BRAND_NAME"), 
+                            F.col("MENU_TYPE"), 
+                            F.col("PRIMARY_CITY"), 
+                            F.col("REGION"), 
+                            F.col("COUNTRY"), 
+                            F.col("FRANCHISE_FLAG"), 
+                            F.col("FRANCHISE_ID"), 
+                            F.col("FRANCHISEE_FIRST_NAME"), 
+                            F.col("FRANCHISEE_LAST_NAME"), 
+                            F.col("LOCATION_ID"), 
+                            F.col("MENU_ITEM_ID"), 
+                            F.col("MENU_ITEM_NAME"), 
+                            F.col("QUANTITY"), 
+                            F.col("UNIT_PRICE"), 
+                            F.col("PRICE"), 
+                            F.col("ORDER_AMOUNT"), 
+                            F.col("ORDER_TAX_AMOUNT"), 
+                            F.col("ORDER_DISCOUNT_AMOUNT"), 
                             F.col("ORDER_TOTAL"))
+    
+    # Crea o reemplaza la vista existente con el dataframe final.
     final_df.create_or_replace_view('POS_FLATTENED_V')
 
+# Define la función para crear el stream de la vista POS.
 def create_pos_view_stream(session):
     session.use_schema('HARMONIZED')
     _ = session.sql('CREATE OR REPLACE STREAM POS_FLATTENED_V_STREAM \
                         ON VIEW POS_FLATTENED_V \
                         SHOW_INITIAL_ROWS = TRUE').collect()
 
+# Define la función para probar la vista POS.
 def test_pos_view(session):
     session.use_schema('HARMONIZED')
     tv = session.table('POS_FLATTENED_V')
     tv.limit(5).show()
 
-
-# For local debugging
+# Esto se ejecutará si el script se ejecuta directamente (no se importa como módulo).
 if __name__ == "__main__":
-    # Add the utils package to our path and import the snowpark_utils function
     import os, sys
     current_dir = os.getcwd()
     parent_dir = os.path.dirname(current_dir)
     sys.path.append(parent_dir)
 
     from utils import snowpark_utils
+    # Obtiene la sesión de Snowpark.
     session = snowpark_utils.get_snowpark_session()
 
+    # Llama a las funciones definidas anteriormente.
     create_pos_view(session)
     create_pos_view_stream(session)
-#    test_pos_view(session)
+    #test_pos_view(session)
 
+    # Cierra la sesión.
     session.close()
